8534964b983ce251e985370e716d3da651a6a1fa
Leo wolf <leo34126001@gmail.com>
2020-12-24 00:28:21 +0800

    Update022
    取消A-1的暗幕
    更改手電筒為常駐開啟，無法以按鍵開關
    刪除手電筒快捷鍵(Q)
    修改地圖閃光點為被手電筒照射時才會顯示
    新增轉場用公用事件
    新增A-2開啟手電筒劇情
    修復部分地圖允許衝刺BUG
    修正部分門可以手持武器開啟BUG
    刪除初次進入全黑場景(A-5)劇情
    修改A-3與A-5交界場景
    增加人形怪AI
    增加消防斧蓄力攻擊
    改良叛逆之劍(素材、判定、設定文字等)
    更改空手時的攻擊力設定為0
    更改道具等說明欄為3行
    戰鬥測試區新增沙包，可測試傷害值
    新增燈光測試區，可以玩尋寶?
    增加文字相關插件
    測試區域可直接跳往B區
    修正取得半自動手槍時的說明文字

--- js/rpg_objects.js
+++ js/rpg_objects.js
@@ -2422,10 +2422,10 @@ Game_BattlerBase.prototype.paramPlus = function(paramId) {
 };
 
 Game_BattlerBase.prototype.paramMin = function(paramId) {
-    if (paramId === 1) {
-        return 0;   // MMP
+    if (paramId === 0) {
+        return 1;   // MHP 以外
     } else {
-        return 1;
+        return 0;
     }
 };
 
