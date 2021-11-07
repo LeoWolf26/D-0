76b924823b5751cc5458c424389454448d57ebf3
Leo wolf <leo34126001@gmail.com>
2021-11-07 13:12:02 +0800

    Update058
    重新啟用滑鼠操作功能
    戰鬥介面增加角色立繪
    增加敵人HP條顯示
    現在當陷入狀態時會有敘述了
    增加急救包和急救包(高級版)，
    增加/變更技能，增加新敵人-怪物3
    更新D-1魚池
    戰鬥測試改放至測試地圖

--- js/rpg_objects.js
+++ js/rpg_objects.js
@@ -3223,7 +3223,7 @@ Game_Battler.prototype.clearTp = function() {
 };
 
 Game_Battler.prototype.chargeTpByDamage = function(damageRate) {
-    var value = Math.floor(50 * damageRate * this.tcr);
+    var value = 0;
     this.gainSilentTp(value);
 };
 
